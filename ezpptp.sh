c78069117cff2789d9b1b94a680dd61be1670c94
iTouchack <itouchack@gmail.com>
2014-10-15 20:09:26 +0300

    Update ezpptp.sh

--- ezpptp.sh
+++ ezpptp.sh
@@ -29,7 +29,7 @@ if test $x -eq 1; then
 	read p
 
 # get the VPS IP
-ip=`ifconfig venet0:0 | grep 'inet addr' | awk {'print $2'} | sed s/.*://`
+ip=`ifconfig eth0:0 | grep 'inet addr' | awk {'print $2'} | sed s/.*://`
 
 echo
 echo "######################################################"
